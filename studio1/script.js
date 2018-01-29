6ee4606cb8c29d11f5918c5c607e42b70e7c58e9
Crystie Sandmire <ctsandmire@ucdavis.edu>
2018-01-28 23:02:55 -0800

    add madlib link
    develop studio 1 further, commit to check css and html links, add madlib link

--- studio1/script.js
+++ studio1/script.js
@@ -1,17 +1,21 @@
+"use strict";
+
+var i = 0;
+
+
+document.f.onsubmit = processForm;
 
 function processForm() {
 
-  var companyname=document.f.companyname.value;
-  var year=document.f.year.value;
-  var numemployees=document.f.numemployees.value;
-  var industry=document.f.industry.value;
-  var jobsite=document.f.jobsite.value;
-  var noun1=["frameworks", "impact", "immersion", "narratives"]
+  var noun1 = ["frameworks", "impact", "immersion", "narratives"]
 
-  var myMsg=document.getElementById("myMsg");
+  var myMsg = document.getElementById("myMsg");
+  myMsg.innerHTML = noun1[i];
+  i++;
 
-  myMsg.innerHTML="At <em>" + companyname + "</em>, we believe in (holistic) " + noun1[0] +". (Eagerly) (pioneering) since <em>" + year + "</em>, <em>"+ companyname + "</em> has grown to a family of <em>" + numemployees + "</em> (innovators) who are constantly (immersing), (diversifying) and (streamlining) (impact). Our mission is to (disrupt) the <em>" + industry + "</em> indusrty.  To join us, visit <em>" + jobsite + "</em> for job openings.";
+  if (i>3) {
+    i=0;
+  }
+
   return false;
 }
-
-document.f.onsubmit = processForm;
