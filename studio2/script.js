e995e08c8a76370e036369e8125b8f970127eb0d
Crystie Sandmire <ctsandmire@ucdavis.edu>
2018-02-03 17:40:32 -0800

    futher studio 2 developments

--- studio2/script.js
+++ studio2/script.js
@@ -1,6 +1,48 @@
 // var sharp = document.getElementById('sharp');
 var wb = document.getElementById('wb');
-var close = document.getElementById('close');
+var plan = document.getElementById('plan');
+var comp = document.getElementById('comp');
+
+var close1 = document.getElementById('close1');
+var close2 = document.getElementById('close2');
+var close3 = document.getElementById('close3');
+
+
+// WATER BOTTLE
+wb.addEventListener('touchstart', function(){
+  overlay1.style.display='block';
+     wb.src='images/wbopen.png';
+
+})
+
+close1.addEventListener('touchstart', function(){
+  overlay1.style.display='none';
+     wb.src='images/wbclosed.png';
+})
+
+// PLANNER
+plan.addEventListener('touchstart', function(){
+  overlay2.style.display='block';
+     plan.src='images/plan2.png';
+
+})
+
+close2.addEventListener('touchstart', function(){
+  overlay2.style.display='none';
+     plan.src='images/plan1.png';
+})
+
+// COMPUTER
+comp.addEventListener('touchstart', function(){
+  overlay3.style.display='block';
+     comp.src='images/compopen.png';
+
+})
+
+close3.addEventListener('touchstart', function(){
+  overlay3.style.display='none';
+     comp.src='images/compclosed.png';
+})
 
 
 
@@ -16,17 +58,6 @@ var close = document.getElementById('close');
 //   sharp.style.cursor = "url(images/sharppoint.png), auto";
 // })
 
-wb.addEventListener('touchstart', function(){
-  overlay.style.display='block';
-     wb.src='images/wbopen.png';
-
-})
-
-close.addEventListener('touchstart', function(){
-  overlay.style.display='none';
-     wb.src='images/wbclosed.png';
-
-})
 
 // wb.addEventListener('mouseover', function(){
 //   wb.src='images/wbopen.png';
