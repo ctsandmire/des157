9b64c92e54e96d3267f6d4bc1a5b35940b6223c7
Crystie Sandmire <ctsandmire@ucdavis.edu>
2018-02-07 22:47:50 -0800

    debug html

--- studio2/script.js
+++ studio2/script.js
@@ -1,4 +1,3 @@
-
 var wb = document.getElementById('wb');
 var plan = document.getElementById('plan');
 var comp = document.getElementById('comp');
@@ -9,129 +8,106 @@ var close3 = document.getElementById('close3');
 
 
 // WATER BOTTLE
-wb.addEventListener('touchstart', function(){
-  overlay1.style.display='block';
-     wb.src='images/wbopen.png';
+wb.addEventListener('touchstart', function() {
+  overlay1.style.display = 'block';
+  wb.src = 'images/wbopen.png';
 
-     overlay2.style.display='none';
-     overlay3.style.display='none';
+  overlay2.style.display = 'none';
+  overlay3.style.display = 'none';
 
-     plan.src='images/plan1.png';
-     comp.src='images/compclosed.png';
+  plan.src = 'images/plan1.png';
+  comp.src = 'images/compclosed.png';
 })
 
-close1.addEventListener('touchstart', function(){
-  overlay1.style.display='none';
-     wb.src='images/wbclosed.png';
+close1.addEventListener('touchstart', function() {
+  overlay1.style.display = 'none';
+  wb.src = 'images/wbclosed.png';
 })
 
 // WATER BOTTLE DESKTOP
-wb.addEventListener('mousedown', function(){
-  overlay1.style.display='block';
-     wb.src='images/wbopen.png';
+wb.addEventListener('mousedown', function() {
+  overlay1.style.display = 'block';
+  wb.src = 'images/wbopen.png';
 
-     overlay2.style.display='none';
-     overlay3.style.display='none';
+  overlay2.style.display = 'none';
+  overlay3.style.display = 'none';
 
-     plan.src='images/plan1.png';
-     comp.src='images/compclosed.png';
+  plan.src = 'images/plan1.png';
+  comp.src = 'images/compclosed.png';
 })
 
-close1.addEventListener('mousedown', function(){
-  overlay1.style.display='none';
-     wb.src='images/wbclosed.png';
+close1.addEventListener('mousedown', function() {
+  overlay1.style.display = 'none';
+  wb.src = 'images/wbclosed.png';
 })
 
 
 // PLANNER
-plan.addEventListener('touchstart', function(){
-  overlay2.style.display='block';
-     plan.src='images/plan2.png';
+plan.addEventListener('touchstart', function() {
+  overlay2.style.display = 'block';
+  plan.src = 'images/plan2.png';
 
-     overlay1.style.display='none';
-     overlay3.style.display='none';
+  overlay1.style.display = 'none';
+  overlay3.style.display = 'none';
 
-     comp.src='images/compclosed.png';
-     wb.src='images/wbclosed.png';
+  comp.src = 'images/compclosed.png';
+  wb.src = 'images/wbclosed.png';
 })
 
-close2.addEventListener('touchstart', function(){
-  overlay2.style.display='none';
-     plan.src='images/plan1.png';
+close2.addEventListener('touchstart', function() {
+  overlay2.style.display = 'none';
+  plan.src = 'images/plan1.png';
 })
 
 // PLANNER DESKTOP
-plan.addEventListener('mousedown', function(){
-  overlay2.style.display='block';
-     plan.src='images/plan2.png';
+plan.addEventListener('mousedown', function() {
+  overlay2.style.display = 'block';
+  plan.src = 'images/plan2.png';
 
-     overlay1.style.display='none';
-     overlay3.style.display='none';
+  overlay1.style.display = 'none';
+  overlay3.style.display = 'none';
 
-     comp.src='images/compclosed.png';
-     wb.src='images/wbclosed.png';
+  comp.src = 'images/compclosed.png';
+  wb.src = 'images/wbclosed.png';
 })
 
-close2.addEventListener('mousedown', function(){
-  overlay2.style.display='none';
-     plan.src='images/plan1.png';
+close2.addEventListener('mousedown', function() {
+  overlay2.style.display = 'none';
+  plan.src = 'images/plan1.png';
 })
 
 // COMPUTER
-comp.addEventListener('touchstart', function(){
-  overlay3.style.display='block';
-     comp.src='images/compopen.png';
+comp.addEventListener('touchstart', function() {
+  overlay3.style.display = 'block';
+  comp.src = 'images/compopen.png';
 
-     overlay2.style.display='none';
-     overlay1.style.display='none';
+  overlay2.style.display = 'none';
+  overlay1.style.display = 'none';
 
-     wb.src='images/wbclosed.png';
-     plan.src='images/plan1.png';
+  wb.src = 'images/wbclosed.png';
+  plan.src = 'images/plan1.png';
 
 })
 
-close3.addEventListener('touchstart', function(){
-  overlay3.style.display='none';
-     comp.src='images/compclosed.png';
+close3.addEventListener('touchstart', function() {
+  overlay3.style.display = 'none';
+  comp.src = 'images/compclosed.png';
 })
 
 // COMPUTER DESKTOP
-comp.addEventListener('mousedown', function(){
-  overlay3.style.display='block';
-     comp.src='images/compopen.png';
+comp.addEventListener('mousedown', function() {
+  overlay3.style.display = 'block';
+  comp.src = 'images/compopen.png';
 
-     overlay2.style.display='none';
-     overlay1.style.display='none';
+  overlay2.style.display = 'none';
+  overlay1.style.display = 'none';
 
-     wb.src='images/wbclosed.png';
-     plan.src='images/plan1.png';
+  wb.src = 'images/wbclosed.png';
+  plan.src = 'images/plan1.png';
 
 })
 
-close3.addEventListener('mousedown', function(){
-  overlay3.style.display='none';
-     comp.src='images/compclosed.png';
+close3.addEventListener('mousedown', function() {
+  overlay3.style.display = 'none';
+  comp.src = 'images/compclosed.png';
 })
-
-
-
-// sharp.addEventListener('mouseover', function(){
-//   sharp.src='images/sharpopen.png';
-// })
-//
-// sharp.addEventListener('mouseout', function(){
-//   sharp.src='images/sharp.png';
-// })
-
-// sharp.addEventListener('mousedown', function(){
-//   sharp.style.cursor = "url(images/sharppoint.png), auto";
-// })
-
-
-// wb.addEventListener('mouseover', function(){
-//   wb.src='images/wbopen.png';
-// })
-//
-// wb.addEventListener('mouseout', function(){
-//   wb.src='images/wbclosed.png';
-// })
