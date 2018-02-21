51fd4401a34404fffeb86b53ec56037a742a9954
Crystie Sandmire <ctsandmire@ucdavis.edu>
2018-02-20 21:36:39 -0800

    further develop studio 4

--- studio4/script.js
+++ studio4/script.js
@@ -30,12 +30,65 @@ prev.addEventListener('click', function(){
 
 document.f.onsubmit = processForm;
 
-
 function processForm(){
+
   var signsearch = document.f.signsearch.value;
 
-  sign.src = 'images/' + signsearch + '.png';
+  for (var i = 0; i < signsearch.length; i++){
+    // clear old string
+    // img.src = "";
+    // check to see if its working
+    console.log(signsearch.charAt(i));
+    // 4 lines follow to  create a new image for each character in the string
+    var img = document.createElement("img");
+    img.src = 'images/' + signsearch.charAt(i) + '.png';
+
+    if (signsearch == "mom" || signsearch == "mother"){
+      img.src = 'images/mom.png';
+    }
+
+    var src = document.getElementById('showsigns');
+    src.appendChild(img);
+
+  }
+  // it goes outside to print all the letters to the console, instead of just 1st
+    return false;
+
 }
+
+
+// function processForm(){
+//   var signsearch = document.f.signsearch.value;
+//
+//   if (signsearch.length == 1){
+//     sign.src = 'images/' + signsearch + '.png';
+//     return false;
+//   }
+//
+//   if (signsearch.length > 1){
+//
+//     for (var i = 0; i < signsearch.length; i++){
+//       //removes default letter image
+//       sign.style.display='none';
+//
+//       console.log(signsearch.charAt(i));
+//
+//       // var img = document.createElement("img");
+//       // img.src = 'images/' + signsearch.charAt(i) + '.png';
+//       // var src = document.getElementById('showsigns');
+//       //
+//       // src.appendChild(img);
+//
+//
+//       //only displaying the last letter in the string as img src
+//       // sign.src = 'images/' + signsearch.charAt(i) + '.png';
+//
+//     }
+//
+//     //it is here to show all the letters of the string instead of just the last
+//     return false;
+//   }
+// }
 
 
 function setup(){
