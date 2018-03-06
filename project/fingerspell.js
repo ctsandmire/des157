070e53620f89c43f36f384d5c1cc25d7c68411c9
Crystie Sandmire <ctsandmire@ucdavis.edu>
2018-03-06 10:51:07 -0800

    add fingerspelling developments

--- project/fingerspell.js
+++ project/fingerspell.js
@@ -1,17 +1,21 @@
-// UNSURE HOW to create a slideshoe-like display for the sign images
-// do display them all in html then hide/show them with js
-// do i create a new img for each sign that needs to be shown
+//
 'use strict';
 
 var letterimg = document.getElementById('letterimg');
 var words = ["hungry", "family", "cat"];
 var next = document.getElementById('next_word_button');
+var check = document.getElementById('check_answer_button');
+
+var user_answer = document.getElementById('answer_input').value;
+
 var order = 0;
 var current_word = words[order];
+var letterImgInt;
+  var i=0;
 
 letterimg.style.display = 'none';
 
-
+// setInterval(spell_word(current_word), 1000);
 //begin game
 start.addEventListener('click', function() {
   //start button goes away
@@ -22,24 +26,44 @@ start.addEventListener('click', function() {
   //show the next array/word
 
   //call the word spelling function using the current_word = words[order] = words [0] = "hungry"
-  spell_word(current_word);
+  // spell_word(current_word);
+  letterImgInt = setInterval(spell_word, 400);
+
+
 })
 
 // this is the main function
 // it takes the current_word, breaks it into letters and uses those to display a series of images
-function spell_word(a){
-  console.log("the word we are spelling is: " + a);
+function spell_word(){
+  // console.log("the word we are spelling is: " + current_word);
   letterimg.style.display = 'block';
-  var i;
-  for (i = 0; i < a.length; i++){
-    var img = document.createElement("img");
-    img.src = 'fimages/' + current_word.charAt(i) + '.svg';
-    img.style = 'height:200px';
+
+  //for (i = 0; i < a.length; i++){
+    // var img = document.getElementById("letterimg");
+    letterimg.src = 'images/' + current_word.charAt(i) + '.svg';
     console.log('current letter: ' + current_word.charAt(i));
-    var src = document.getElementById('spelling_area');
-    src.appendChild(img);
-  }
+    // var src = document.getElementById('spelling_area');
+    // src.appendChild(img);
+    i++;
+    if (i > current_word.length){
+      letterimg.src = 'images/replay.svg';
+      letterimg.style.cursor = "pointer";
+      letterimg.addEventListener('click', function(){
+        i=0;
+      })
+    }
+  //}
 }
+
+check.addEventListener('click', function(){
+  clearInterval(letterImgInt);
+  console.log('value entered is:' + user_answer);
+  //take what is entered
+  //turn it into lowercase
+  //check if its the ame as current_word
+  //if yes display correct signal (and possibly go to the next word)
+  //if no display incorrect signal
+})
 
 
 //when you click the next button, it runs the function that changes words array to the next word in the array, aka adds one to order and therefore changes current_word
@@ -49,7 +73,8 @@ next.addEventListener('click', function(){
     console.log("the word is now: " + words[order]);
     //re-establish(?) current_word so it changes when order changes
     current_word = words[order];
-    spell_word(current_word);
+    // spell_word(current_word);
+    setInterval(spell_word(current_word), 600);
 
     // use this, above, later to make it stop at the end of the word bank
     // for(var i=0; i < words.length; i++){}
